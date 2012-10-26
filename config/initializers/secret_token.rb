5b5df940ceffff58272e3728a425eeb98bf31263
Shoma Nishikawa <shoma.nishikawa@gmail.com>
2012-10-25 20:58:22 -0400

    started rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twittabot::Application.config.secret_token = '00f4e2a0d9e6f394c76cad976ca286c740ba277fd289ed1569038935efd28444209bfbc6e5245dedabbb8fa0fa6d269b306aca996d74b04e1e1f73284372c097'
